facde2b34abb75bff1c18799296e888760897c13
AaronC-DIT <c15489458@mydit.ie>
2018-12-03 16:07:14 +0000

    Create run_split.py

---
+++ run_split.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+from train_test_split import *
+
+# Change these paths to match the location of the CUB dataset on your machine 
+img_train_test_split("C:/Users/aaron/Documents/FYP/CUB_200_2011/images",0.1)
